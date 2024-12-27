77f224e820571778092ee200f9571a1a4a0ae77e
Thiago Mattos <thiagohrm@gmail.com>
2024-12-27 08:22:31 -0300

    Adding functionality to hide or show UI views based on number of profiles on database

--- app/src/main/java/com/senai/soundsetting/MainActivity.kt
+++ app/src/main/java/com/senai/soundsetting/MainActivity.kt
@@ -1,23 +1,50 @@
 package com.senai.soundsetting
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.senai.soundsetting.profile.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: ProfileViewModel by viewModels()
+    private lateinit var bottomBar: BottomNavigationView
+    private var nbOfProfiles = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(this::class.simpleName, "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         //Configura o menu inferior de acordo com a grafo de navegacao criado em res/navigation/nav_graph
-        val bottomBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        onNumberOfProfilesChanged(nbOfProfiles)
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         NavigationUI.setupWithNavController(bottomBar, navController)
+
+        viewModel.profiles.observe(this) {
+                profiles ->
+            nbOfProfiles = profiles?.size ?: 0
+            onNumberOfProfilesChanged(nbOfProfiles)
+        }
+    }
+
+    private fun onNumberOfProfilesChanged(newNumberOfProfiles: Int){
+        Log.i(this::class.simpleName, "onNumberOfProfilesChanged $newNumberOfProfiles")
+        if (newNumberOfProfiles == 0 ){
+            bottomBar.menu.findItem(R.id.profileFragment).isVisible = true
+            bottomBar.menu.findItem(R.id.bmtFragment).isVisible = false
+            bottomBar.menu.findItem(R.id.volumeFragment).isVisible = false
+        } else {
+            bottomBar.menu.findItem(R.id.profileFragment).isVisible = true
+            bottomBar.menu.findItem(R.id.bmtFragment).isVisible = true
+            bottomBar.menu.findItem(R.id.volumeFragment).isVisible = true
+        }
     }
 }
--- app/src/main/java/com/senai/soundsetting/data/persistance/PersistanceManager.kt
+++ app/src/main/java/com/senai/soundsetting/data/persistance/PersistanceManager.kt
@@ -1,4 +1,18 @@
 package com.senai.soundsetting.data.persistance
 
-class PersistanceManager {
+import android.content.Context
+import javax.inject.Inject
+
+class PersistanceManager @Inject constructor(private val context: Context) {
+    private val sharedPreferences = context.getSharedPreferences("AudioSettingProfile", Context.MODE_PRIVATE)
+    private val editor = sharedPreferences.edit()
+    fun saveData(key: String, value: String) {
+        editor.putString(key, value)
+        editor.apply()
+    }
+    fun getData(key: String, defaultValue: String): String {
+        return sharedPreferences.getString(key, defaultValue) ?: defaultValue
+    }
+
+
 }
